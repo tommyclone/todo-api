34cfdf0e9af292ac257630d3c3b06aa1d6833eee
tommyclone <tommyclone@gmail.com>
2017-01-30 23:34:36 +0800

    todo api get

--- server.js
+++ server.js
@@ -1,6 +1,37 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+    id: 1,
+    description: 'Meet mum for lunch',
+    completed: false
+}, {
+    id: 2,
+    description: 'Go to market',
+    completed: false,
+}, {
+    id: 3,
+    description: 'Feed cat',
+    completed: true
+}];
+
+// GET /todos
+app.get('/todos', function(req, res){
+    res.json(todos);
+});
+// GET /todos/:id
+app.get('/todos/:id', function(req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var todo = todos.find(function(value, index){
+        return value.id == todoId;
+    });
+    if (!todo) {
+        res.status(404).json({ 
+            error: 'todo not found.'
+        });
+    }
+    res.json(todo);
+});
 
 app.get('/', function(req, res) {
     res.send('Todo API Root');
