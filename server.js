c9f89af4ff2ce6b0774b86b4a2df4f16ec69c0cc
tommyclone <tommyclone@gmail.com>
2017-01-31 20:28:42 +0800

    add put method

--- server.js
+++ server.js
@@ -41,6 +41,38 @@ app.get('/', function(req, res) {
     res.send('Todo API Root');
 });
 
+// PUT /todos/:id
+app.put('/todos/:id', function(req, res){
+
+    var todoId = parseInt(req.params.id, 10);
+    var todo = _.findWhere(todos, {id: todoId});
+    if (!todo) {
+        return res.status(404).json({ 
+            error: 'todo not found.'
+        });
+    }
+
+    var body = req.body;
+    body = _.pick(body, 'description', 'completed');
+    var validAttributes = {};
+    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+        validAttributes.completed = body.completed;
+    } else if(body.hasOwnProperty('completed')) {
+        return res.status(400).json({"error": "completed attribute must be boolean"});
+    }
+    else {}
+
+    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+        validAttributes.description = body.description;
+    } else if(body.hasOwnProperty('description')) {
+        return res.status(400).json({"error": "description attribute must non-empty string"});
+    }
+    else {}
+
+    todo = _.extend(todo, validAttributes)
+
+    res.json(todo);
+})
 // POST /todos
 app.post('/todos', function(req, res) {
     var body = req.body;
