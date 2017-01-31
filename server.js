40ceaa738e27bce94b171e884b9e662d04314d55
tommyclone <tommyclone@gmail.com>
2017-01-31 18:51:38 +0800

    Added post request to add todo

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-    id: 1,
-    description: 'Meet mum for lunch',
-    completed: false
-}, {
-    id: 2,
-    description: 'Go to market',
-    completed: false,
-}, {
-    id: 3,
-    description: 'Feed cat',
-    completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 // GET /todos
 app.get('/todos', function(req, res){
@@ -35,6 +27,19 @@ app.get('/todos/:id', function(req, res) {
 
 app.get('/', function(req, res) {
     res.send('Todo API Root');
+});
+
+// POST /todos
+app.post('/todos', function(req, res) {
+    var body = req.body;
+    var newTodo = {        
+        id: todoNextId++,
+        description: body.description,
+        completed: body.completed
+    };
+    todos.push(newTodo);
+    res.status(201).json(newTodo);
+    
 });
 
 app.listen(PORT, function() {
