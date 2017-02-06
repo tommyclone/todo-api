51cd78993f30b8784b9f9702644f2593be217cd9
tommyclone <tommyclone@gmail.com>
2017-02-06 23:55:21 +0800

    delete method

--- server.js
+++ server.js
@@ -68,14 +68,19 @@ app.get('/todos/:id', function(req, res) {
 // DELETE /todos/:id
 app.delete('/todos/:id', function(req, res){
     var todoId = parseInt(req.params.id, 10);
-    var todo = _.findWhere(todos, {id: todoId});
-    if (!todo) {
-        return res.status(404).json({ 
-            error: 'todo not found.'
-        });
-    }
-    todos = _.without(todos, todo);
-    res.json(todo);
+    db.todo.destroy({
+        where: {
+            id: todoId
+        }
+    }).then(function(rowsDeleted){
+        if (rowsDeleted === 0) {
+            res.status(404).json({'error': 'todo not found'});
+        } else {
+            res.status(204).send();
+        }
+    }, function(e) {
+        res.status(500).json(e);
+    });
 });
 
 app.get('/', function(req, res) {
